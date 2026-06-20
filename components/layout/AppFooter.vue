3eb844a90cadc5cbcc7ccf319562b98af13a68d5
Konrad Hugo <konradcoding@gmail.com>
2026-06-20 13:56:43 -0700

    fuckn footer logo

--- components/layout/AppFooter.vue
+++ components/layout/AppFooter.vue
@@ -28,7 +28,7 @@ import { footerSections, footerMeta } from '~/data/footer.links'
           >
             {{ footerMeta.cta.title }}
           </v-btn> -->
-          <v-img src="/IRIE_Web_Logo.gif" max-height="120" max-width="120"></v-img>
+          <v-img src="/public/IRIE_Web_Logo.gif" max-height="120" max-width="120"></v-img>
 
           <!-- SOCIALS -->
           <div class="mt-4 d-flex ga-2">
