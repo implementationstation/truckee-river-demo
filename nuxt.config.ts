ba27b1ed2b6f7eb0c4df4f391f44210f6408d6c8
Konrad Hugo <konradcoding@gmail.com>
2026-06-27 23:56:08 -0700

    rerouting 301 links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,51 +1,51 @@
 export default defineNuxtConfig({
+  routeRules: {
+    '/lake-tahoe-attractions-activities-lodging': {
+      redirect: { to: '/', statusCode: 301 },
+    },
 
-  /*
-  ========================
-  GLOBAL DEFAULTS
-  ========================
-  */
- app: {
+    '/truckee-river-rafting-family-rafting-trips-near-truckee-north-lake-tahoe': {
+      redirect: { to: '/', statusCode: 301 },
+    },
+
+    '/pricing-cancellations-refunds-gratuities': {
+      redirect: { to: '/', statusCode: 301 },
+    },
+
+    '/northern-california-whitewater-rafting-trips': {
+      redirect: { to: '/', statusCode: 301 },
+    },
+
+    '/lake-tahoe-rafting-tours-truckee-river': {
+      redirect: { to: '/', statusCode: 301 },
+    },
+    '/reservation-information': {
+      redirect: { to: '/', statusCode: 301 },
+    },
+  },
+
+  app: {
     head: {
       title: 'Truckee Tahoe Guided Whitewater Rapid Rafting Trips',
       meta: [
-        { 
-          name: 'description', 
-          content: 'Experience the most popular rafting section near Tahoe on a guided half-day adventure through fun Class II-III rapids, calm scenic stretches, and beautiful Sierra mountain views.' }
-      ]
-    }
+        {
+          name: 'description',
+          content:
+            'Experience the most popular rafting section near Tahoe on a guided half-day adventure through fun Class II-III rapids, calm scenic stretches, and beautiful Sierra mountain views.',
+        },
+      ],
+    },
   },
-
-    /*
-  ========================
-  MODULES
-  ========================
-  */
 
   modules: ['vuetify-nuxt-module', '@nuxt/image'],
 
-  /*
-  ========================
-  GLOBAL CSS
-  ========================
-  */
-
-  css: [
-    '@mdi/font/css/materialdesignicons.css'
-  ],
-
-  /*
-  ========================
-  VUETIFY SETTINGS
-  ========================
-  */
+  css: ['@mdi/font/css/materialdesignicons.css'],
 
   vuetify: {
     vuetifyOptions: {
       theme: {
-        defaultTheme: 'light'
-      }
-    }
-  }
-
+        defaultTheme: 'light',
+      },
+    },
+  },
 })
